8979e04e480e74ddcd4cc2395d568da1f1ebe1c0
Ge0rges <ge0rges@ge0rges.com>
2017-03-17 18:59:59 +0200

    Initial Commit

---
+++ Divity/Divity.h
@@ -0,0 +1,13 @@
+//
+//  Divity.h
+//  Divity
+//
+//  Created by Georges Kanaan on 17/03/2017.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Divity : NSObject
+
+@end
---
+++ Divity/Divity.m
@@ -0,0 +1,13 @@
+//
+//  Divity.m
+//  Divity
+//
+//  Created by Georges Kanaan on 17/03/2017.
+//
+//
+
+#import "Divity.h"
+
+@implementation Divity
+
+@end
