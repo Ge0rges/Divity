5ff0071180891fde3fda60fd103a3bcae518fb15
Ge0rges <ge0rges@ge0rges.com>
2017-03-18 11:30:25 +0200

    Initial Architecture.

--- Divity/Divity.h
+++ Divity/Divity.h
@@ -10,4 +10,49 @@
 
 @interface Divity : NSObject
 
+typedef enum NSMotionType : NSUInteger {
+  NSMotionSport = 0,
+  NSMotionIdle,
+  NSMotionMoving,
+  
+} NSMotionType;
+
+typedef enum NSTransportationMode : NSUInteger {
+  NSTransportationModeWalking = 0,
+  NSTransportationModeRunning,
+  NSTransportationModeCycling,
+  NSTransportationModeCar,
+  NSTransportationModeTrain,
+  NSTransportationModeBoat,
+  NSTransportationModeHelicopter,
+  NSTransportationModePlane,
+  
+} NSTransportationMode;
+
+typedef enum NSWeatherState : NSUInteger {//https://openweathermap.org/weather-conditions
+  NSWeatherStateClear = 0,
+  NSWeatherStateFoggy,
+  NSWeatherStateCloudy,
+  NSWeatherStateRain,
+  NSWeatherStateSnow,
+  NSWeatherStateDrizzle,
+  NSWeatherStateThunderstorm,
+  NSWeatherStateExtreme,
+  NSWeatherStateOther,
+  
+} NSWeatherState;
+
+typedef void (^handler)(Divity *currentDivity);
++ (void)calculateCurrentDivity:(handler)handler;
+
+@property (readonly, nonatomic) NSMotionType motionType;
+@property (readonly, nonatomic) NSTransportationMode transportationMode;
+@property (readonly, nonatomic) NSWeatherState weather;
+@property (readonly, nonatomic) BOOL playingMusic;
+@property (readonly, nonatomic) BOOL usingHeadphones;
+@property (strong, readonly, nonatomic) NSDictionary *place;// Courtesy of Foursquare
+
+// Activity is based upon all of the above. It's an action in english.
+@property (strong, readonly, nonatomic) NSString *likelyActivity;
+
 @end
--- Divity/Divity.m
+++ Divity/Divity.m
@@ -8,6 +8,42 @@
 
 #import "Divity.h"
 
+@interface Divity ()
+
+@property (nonatomic) NSMotionType motionType;
+@property (nonatomic) NSTransportationMode transportationMode;
+@property (nonatomic) NSWeatherState weather;
+@property (nonatomic) BOOL playingMusic;
+@property (nonatomic) BOOL usingHeadphones;
+@property (strong, nonatomic) NSDictionary *place;// Courtesy of Foursquare
+
+// Activity is based upon all of the above. It's an action in english.
+@property (strong, nonatomic) NSString *likelyActivity;
+
+@end
+
 @implementation Divity
+
+// Different datapoints to build a divity upon:
+
+// On board data
+//NSDivitiWeather = 0,
+//NSDivityMotion,// Running, walking, car, train, plane...
+//NSDivityMicrophoneLevel,// Used to narrow down places eg bar or park.
+//NSDivityCalendar,// Currently in a meeting?
+//NSDivityReminders,// Investigate.
+//NSDivityHomeKit,// Investigate.
+//NSDivityHealthKit,// Heart rate
+//NSDivityMusic,// Listenning to music or not.
+//NSDivityTime,
+//NSDivityAltitude
+//
+//// 3rd Party Services & Apps
+//NSDivityUber,// In a ride or not.
+//NSDivityFoursquare,// This renders location useful by giving it context.
+
++ (void)calculateCurrentDivity:(handler)handler {
+  
+}
 
 @end
